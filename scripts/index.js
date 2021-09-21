d064ec6e24799f121ccc8da2a4c6601c6fdda3f6
SamuelDavidChristensen <84264717+SamuelDavidChristensen@users.noreply.github.com>
2021-09-20 20:36:24 -0700

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,17 @@
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+
 // Modal Image Gallery
 function onClick(element) {
   document.getElementById("img01").src = element.src;
